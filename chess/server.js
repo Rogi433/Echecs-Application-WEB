ce10837d25a85698d75bdd195e5a87f240148aca
Victor <victor.bayona@ecl18.ec-lyon.fr>
2020-03-25 15:26:51 +0100

    ajout d'une page de création/sélection de partie

--- chess/server.js
+++ chess/server.js
@@ -6,11 +6,79 @@ var port = process.env.PORT || 8080;
 
 app.get('/*',express.static(__dirname + '/public'));
 
+var plateaux=[];
+var noms=[];
+var socketsBlanc=[];
+var socketsNoir=[];
+
+var current_id=0;
 
 var socketBlanc=undefined;
 var socketNoir=undefined;
 
 io.on('connection', function(socket){
+	
+	socket.emit('listeParties',JSON.stringify(noms));
+	
+	socket.on('createGame',function(nom,fnc){
+		fnc(current_id,nom);
+		plateaux.push(initBoard());
+		noms.push(nom);
+		socketsBlanc.push(undefined);
+		socketsNoir.push(undefined);
+		current_id++;
+	});
+	
+	socket.on('joinGame',function(id,joueur){
+		console.log("join");
+		socket.id=id;
+		socket.joueur=joueur;
+		socket.emit('init',joueur,JSON.stringify(plateaux[id]));
+		if (joueur==1){
+			socketsBlanc[id]=socket;
+			
+		}
+		else{
+			socketsNoir[id]=socket;
+		}
+		
+		console.log("connecté");
+		
+		
+		socket.on('disconnect',function(){
+			console.log("Déconnecté");
+		});
+		
+		socket.on('update',function(data){
+			board=JSON.parse(data);
+			
+			if (socket.joueur==1){
+				socketsNoir[socket.id].emit('update',data);
+			}
+			else{
+				socketsBlanc[socket.id].emit('update',data);
+			}
+		});
+		
+		socket.on('chat',function(message){
+			if (socket.joueur==1){
+				socketsNoir[socket.id].emit('chat',message);
+			}
+			else{
+				socketsBlanc[socket.id].emit('chat',message);
+			}
+		});
+		
+		socket.on('end',function(data,gagnant){
+			if (socket.joueur==1){
+				socketsNoir[socket.id].emit('end',data,gagnant);
+			}
+			else{
+				socketsBlanc[socket.id].emit('end',data,gagnant);
+			}
+		});
+	});
+	/*
 	if (socketBlanc==undefined){
 		socketBlanc=socket;
 		console.log("Blanc connecté");
@@ -69,31 +137,36 @@ io.on('connection', function(socket){
 		socket.disconnect()
 		console.log('Disconnected')
 	}
+	*/
 });
+
 
 
 server.listen(port);
 
+function initBoard(){
+	let board;
+	//initialisation du jeu d'echec
+	let ligne1=[-5,-4,-3,-2,-1,-3,-4,-5];
+	let ligne2=[-6,-6,-6,-6,-6,-6,-6,-6];
 
-//initialisation du jeu d'echec
-var ligne1=[-5,-4,-3,-2,-1,-3,-4,-5];
-var ligne2=[-6,-6,-6,-6,-6,-6,-6,-6];
+	board=[ligne1,ligne2];
+	for (let k=0;k<4;k++){
+		let ligne=[0,0,0,0,0,0,0,0];
+		board.push(ligne);
+	}
 
-var board=[ligne1,ligne2];
-for (var k=0;k<4;k++){
-	let ligne=[0,0,0,0,0,0,0,0];
-	board.push(ligne);
+	let newligne1 =[];
+	let newligne2=[];
+	for (let k=0;k<8;k++){
+		newligne1.push(-ligne1[k]);
+		newligne2.push(-ligne2[k]);
+	}
+	board.push(newligne2);
+	board.push(newligne1);
+	
+	return board
 }
-
-var newligne1 =[];
-var newligne2=[];
-for (var k=0;k<8;k++){
-	newligne1.push(-ligne1[k]);
-	newligne2.push(-ligne2[k]);
-}
-board.push(newligne2);
-board.push(newligne1);
-
 
 
 
